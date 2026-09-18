86b67dafe4616e39312da48157f0a0492b9625db
Nicolas <n178372@gmail.com>
2026-09-18 08:45:57 -0300

    feat: banco de dados do projeto

--- src/components/Pedido.jsx
+++ src/components/Pedido.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react"
 
+//Arrat de objetos contendo o estado inicial do cardapio
+const cardapio={
+    {id:1,nome:"Combo-01",preco:25.00,disponivel:true,quantidade:0}
+    {id:2,nome:"Combo-02",preco:35.00,disponivel:true,quantidade:0}
+    {id:3,nome:"Combo-03",preco:40.00,disponivel:false,quantidade:0}
+    {id:4,nome:"Combo-04",preco:65.00,disponivel:true,quantidade:0}
 
 const Pedido = () => {
   return (
